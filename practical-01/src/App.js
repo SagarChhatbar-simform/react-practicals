799061a07974fd59628017494ef87b7fc59e8211
Sagar Chhatbar <96295134+SagarChhatbar-simform@users.noreply.github.com>
2022-02-22 13:42:44 +0530

    Update App.js

--- practical-01/src/App.js
+++ practical-01/src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           <h2>Hello World</h2>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
