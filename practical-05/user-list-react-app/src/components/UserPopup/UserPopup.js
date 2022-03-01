7033cd8a48dca635d27b74fc261b0e8fb38bb175
SagarChhatbar-simform <sagar.chhatbar@simformsolutions.com>
2022-03-01 17:46:31 +0530

    Practical-05

---
+++ practical-05/user-list-react-app/src/components/UserPopup/UserPopup.js
@@ -0,0 +1,45 @@
+import styles from './UserPopup.module.css';
+
+
+const UserPopup = (props) => {
+
+
+    console.log();
+    return (
+        <div className={styles.Popupdiv} >
+            <img src={props.listId.image} alt='' />
+            <h4>
+                {props.listId.name}
+            </h4>
+            <p>
+                {props.listId.email}
+
+            </p>
+            <h4>Your Plan : Standard</h4>
+            <button>Active User</button>
+
+            <h5>Plan Uses</h5>
+
+            <div className={styles.prog}>
+            </div>
+
+            <div className={styles.numbersParent}>
+                <div>
+                    <h2>
+                        {props.listId.clicksReviewed}
+                    </h2>
+                    <p>Clicks Reviewed</p>
+                </div>
+                <hr />
+                <div>
+                    <h2>
+                    {props.listId.monthlyClicks}
+                    </h2>
+                    <p>Monthly Clicks</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserPopup;
